5737976495e9a74a5f4748e1a4db7185e6aa1aa9
Kevin Lee <kevinmlee.x@gmail.com>
2014-04-05 12:52:46 -0700

    inital commit with flask skeleton

---
+++ playapp/views.py
@@ -0,0 +1,6 @@
+from flask import render_template
+from playapp import app
+
+@app.route('/')
+def index():
+	pass
---
+++ run.py
@@ -0,0 +1,5 @@
+from flask import Flask
+
+from playapp import app
+
+app.run(debug = True)
