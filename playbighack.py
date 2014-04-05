07cd37dc85beb16e7c8e7156dd57bf0a7d9f6791
Kevin Lee <kevinmlee.x@gmail.com>
2014-04-05 15:38:02 -0700

    get baseline working

--- playbighack.py
+++ playbighack.py
@@ -3,11 +3,11 @@ from gevent import monkey
 
 monkey.patch_all()
 
-app = Flask(__name__)
-app.debug = True
-app.config['PORT'] = 5000
+application = Flask(__name__)
+application.debug = True
+application.config['PORT'] = 5000
 
-@app.route('/', methods=['GET'])
+@application.route('/', methods=['GET'])
 def index():
 	return render_template("index.html")
 
