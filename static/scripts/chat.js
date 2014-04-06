5659ea4479efb17de5995a71efed0f54156518be
Kevin Lee <kevinmlee.x@gmail.com>
2014-04-05 22:46:10 -0700

    flattened js, working api call on video play

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -1,103 +1,162 @@
-(function($,window){
+// courtesy of youtube api
 
-    var chatAPI = {
-        connect : function(done) {
-            // call the other guy's onMessage
-            var that = this;
+// load iframe api js
+var tag = document.createElement('script');
 
-            this.socket = io.connect('/play');
-            this.socket.on('connect', done);
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-            this.socket.on('message', function(msg) {
-                if(that.onMessage) {
-                    that.onMessage(msg);
-                }
-            });
+// make iframe
+var player;
+function onYouTubeIframeAPIReady() { // execute as soon as api done
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: '40mQRo7MoLA',
+    events: {
+      'onReady' : onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+  globals.player = player;
+}
 
-            this.socket.on('playback', function(play) {
-                alert("received an api call");
-                if(that.onPlayBack) {
-                    that.onPlayBack(play);
-                }
-            });
-        },
+function onPlayerReady(event) {
+  // event.target.playVideo();
+}
 
-        join : function(email, onJoin) {
-            this.socket.emit('join', email, onJoin);
-        },
+function stopVideo() {
+  player.stopVideo();
+}
 
-        sendMessage : function(msg, onSent) {
-            this.socket.emit('message', msg, onSent);
-        },
 
-        sendPlayback : function(play, onPlay) {
-            this.socket.emit('playback', play, onPlay);
+// okay so the above iframe ready function needs
+// to be exposed globally for YT
+// but then the below chat code was enclosed
+// in an anon func i was calling for speed
+// but even when passing in the player
+// player is nul until the above iframe ready f
+// is called so the add listener call won't go through
+// and onplayerstate needs to be in the same scope
+// as chat API
+function onPlayerStateChange(event) {
+    if(event.data == YT.PlayerState.PLAYING) {
+        alert("sending playing api call");
+        chatAPI.sendPlayback( true,
+            function(played, play) {
+                if(played) {
+                //update UI
+            }
+        });
+    }
+}
+
+// chat code
+
+var chatAPI = {
+    connect : function(done) {
+        // call the other guy's onMessage
+        var that = this;
+
+        this.socket = io.connect('/play');
+        this.socket.on('connect', done);
+
+        this.socket.on('message', function(msg) {
+            if(that.onMessage) {
+                that.onMessage(msg);
+            }
+        });
+
+        this.socket.on('playback', function(play) {
+            alert("received an api call");
+            if(that.onPlayBack) {
+                that.onPlayBack(play);
+            }
+        });
+    },
+
+    join : function(email, onJoin) {
+        this.socket.emit('join', email, onJoin);
+    },
+
+    sendMessage : function(msg, onSent) {
+        this.socket.emit('message', msg, onSent);
+    },
+
+    sendPlayback : function(play, onPlay) {
+        this.socket.emit('playback', play, onPlay);
+    }
+};
+
+var bindUI = function() {
+    $(".join-chat").validate({
+        submitHandler: function(form) {
+            chatAPI.join( $(form).find("[name='email']").val(),
+                function(joined, name) {
+                    if(joined) {
+                        alert("you've joined chat");
+                        $(form).hide();
+                        $(".compose-message-form").show();
+                        $(".messages").show();
+                    }
+                });
         }
+    });
+
+    $(".compose-message-form").validate({
+        submitHandler: function(form) {
+            chatAPI.sendMessage( $(form).find("[name='message']").val(),
+                function(sent, msg) {
+                    if(sent) {
+                        $(".messages").append(
+                            jQuery("<li>").html(
+                                "<b>Me</b>: " + msg
+                            )
+                        );
+                    }
+                });
+        }
+    });
+
+    chatAPI.onMessage = function(msg) {
+        $(".messages").append(
+            jQuery("<li>").html(
+                "<b>"+msg.sender+"</b>"+msg.content
+            )
+        );
     };
 
-    var bindUI = function() {
-        $(".join-chat").validate({
-            submitHandler: function(form) {
-                chatAPI.join( $(form).find("[name='email']").val(),
-                    function(joined, name) {
-                        if(joined) {
-                            alert("you've joined chat");
-                            $(form).hide();
-                            $(".compose-message-form").show();
-                            $(".messages").show();
-                        }
-                    });
-            }
-        });
+    $(".playercontrols").validate({
+        submitHandler: function(form) {
+            chatAPI.sendPlayback( true, 
+                function(played, play) {
+                    if(played) {
 
-        $(".compose-message-form").validate({
-            submitHandler: function(form) {
-                chatAPI.sendMessage( $(form).find("[name='message']").val(),
-                    function(sent, msg) {
-                        if(sent) {
-                            $(".messages").append(
-                                jQuery("<li>").html(
-                                    "<b>Me</b>: " + msg
-                                )
-                            );
-                        }
-                    });
-            }
-        });
+                    }
+                });
+        }
+    });
 
-        chatAPI.onMessage = function(msg) {
-            $(".messages").append(
-                jQuery("<li>").html(
-                    "<b>"+msg.sender+"</b>"+msg.content
-                )
-            );
-        };
-
-        $(".playercontrols").validate({
-            submitHandler: function(form) {
-                chatAPI.sendPlayback( true, 
-                    function(played, play) {
-                        if(played) {
-
-                        }
-                    });
-            }
-        });
-
-        chatAPI.onPlayBack = function(play) {
-            alert("someone else played");
-        };
+    chatAPI.onPlayBack = function(play) {
+        alert("someone else played");
     };
+};
 
-    var ready = function() {
+var ready = function() {
 
-        bindUI();
+    bindUI();
 
-        console.log("Play.");
+    console.log("Play.");
 
-        chatAPI.connect(function(){});
-    };
+    chatAPI.connect(function(){});
+};
 
 
-    $(function() { ready(); } );
-}($,window));
+$(function() { ready(); } );
+
+// $(window).ready( function() {
+//     player.addEventListener('onStateChange', onPlayerStateChange);
+// });
+
+// }($, jQuery, window, player));
